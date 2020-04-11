515093128298ba179902d6116401c9bcdb532e91
dragan petkovski <draganpetkovski7@gmail.com>
2020-04-11 19:50:13 +0200

    new branch

---
+++ Course.java
@@ -0,0 +1,30 @@
+class Course{
+list<Student>
+
+public Course(){};
+
+public double prosek(list<Integer> li, Float avg) {
+		int total = 0;
+   		 int avg;
+    	for(int i = 0; i < li.size(); i++){    
+   	    
+        		avg = total / li.size();
+		}	
+	
+		return avg;
+	}
+
+	public integer brojPotpisi(LinkedList<integer>li) {
+	for(integer i=0;i<li.size;i++){
+		if(li[i]>=8)
+		{
+			 total += li.get(i);
+	
+		}
+	return total;	
+		
+	}
+
+
+
+}
