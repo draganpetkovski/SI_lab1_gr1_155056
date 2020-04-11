95f8ec4fc553772e76468175fa00e33c21c7fda4
dragan petkovski <draganpetkovski7@gmail.com>
2020-04-11 18:52:12 +0200

    lab points variable added

--- Student.java
+++ Student.java
@@ -14,4 +14,17 @@ class Student {
 	public boolean hasSignature() {
 		//TODO
 	}
+
+        LinkedList labpoints= new LinkedList(); 
+	labpoints= insert( 1); 
+        list = insert(2); 
+        list = insert( 3); 
+        list = insert( 4); 
+        list = insert( 5); 
+        list = insert( 6); 
+        list = insert(7); 
+        list = insert( 8); 
+
+
+
 }
