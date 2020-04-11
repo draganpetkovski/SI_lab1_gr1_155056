231325b6e32baa47c07cf657d60dd422d259b8d4
dragan petkovski <draganpetkovski7@gmail.com>
2020-04-11 19:31:26 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -3,16 +3,54 @@ class Student {
 	String firstName;
 	String lastName;
 
-	//TODO constructor
+	
 
-	//TODO seters & getters
 
-	public double getAverage() {
-		//TODO
+
+public Student(String index, String firstName, String lastName){
+	this.index=index;
+	this.firstName=firstName;
+	this.lastName=lastName;
 	}
 
-	public boolean hasSignature() {
-		//TODO
+	    public void setIndex(String index){
+      	  this.index= index;
+  	  }
+		
+	 public void setfirstName(String firstName){
+       	 this.index= index;
+ 	   }
+		
+	public void setlastName(String lastName){
+	this.lastName=lastName;	
+	}
+	
+	public String getIndex(){return index;}
+
+	public String getfirstName){return firstName;}
+	
+	public String getlastName){return lastName;}
+
+
+
+	public double getAverage(LinkedList<Integer> li, Float avg) {
+		int total = 0;
+   		 int avg;
+    	for(int i = 0; i < li.size(); i++){
+    
+   	     total += li.get(i);
+        		avg = total / li.size();
+		}	
+	
+		return avg;
+	}
+
+	public boolean hasSignature(LinkedList<integer>li) {
+	for(integer i=0;i<li.size;i++){
+		if(li[i]>=8)
+		return true;
+		
+		
 	}
 
         LinkedList labpoints= new LinkedList(); 
